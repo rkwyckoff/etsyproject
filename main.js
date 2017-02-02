382680ec38ff1477a71363e41cf4b8edc2e1a3b5
Kurt Wyckoff <kurtitservices@gmail.com>
2017-02-02 17:56:41 -0500

    first jas problem solved

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 //write a function that loops through all var "items"
 //it pulls in all the "price" values and divides by items.length
 
-function getPrice(items) {
+var avg = 0;
+
+
+function getPrice() {
   var total = 0;
-  for (count = 0; count < items.length; count++) {
+    for (var count = 0; count < items.length; count++) {
     var data = items[count].price;
     total += data;
   }
-    console.log(total / items.length)
+     avg = (total / items.length);
+     avg = avg.toFixed(2)
+    //console.log(avg);
+
   }
+getPrice();
+var jsAnswer = document.getElementById("answer1");
+jsAnswer.innerHTML = "The average price is $" + avg;
+//console.log(jsAnswer.innerHTML)
+//now take this answer and get it to inner html answer 1
+var numb = 123.23454;
+numb = numb.toFixed(2);
