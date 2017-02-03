8deeaf88866efd6520d0a17ff436a154c8b13fa7
Kurt Wyckoff <kurtitservices@gmail.com>
2017-02-02 23:31:38 -0500

    second problm complete

--- main.js
+++ main.js
@@ -22,36 +22,36 @@ jsAnswer.innerHTML = "The average price is $" + avg;
 //now take this answer and get it to inner html answer 1
 //var numb = 123.23454;
 //numb = numb.toFixed(2);
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
 
 //Problem 2
-//find all items that have currency code : USD
-//write for loop
-//then fetch the prices of these items
- //grab all prices that have a price between $14 and $18
 //write for loop
 
 
+var list = [];
+var jsAnswer = document.getElementById("answer2");
 
-
-
-var avg = 0;
-
-
-function getPrice() {
-  var total = 0;
-    for (var count = 0; count < items.length; count++) {
-    var data = items[count].currency_code;
-    total += data;
+function priceInRange() {
+  // Step 1: For loop goes through list of items
+  for (var i = 0; i < items.length; i++) {
+    // grab the currency code from each item
+    var currencyCode = items[i].currency_code;
+    // grab the price of each item
+    var price = items[i].price;
+    // find all items that have currency code : USD
+    // && all prices between $14 and $18
+    if (currencyCode === 'USD' &&
+        price >= 14.00 &&
+        price <= 18.00) {
+      list.push(items[i].title);
+    }
   }
-     avg = (total / items.length);
-     avg = avg.toFixed(2)
-    //console.log(avg);
+  // Step 2: Pushing the updated list to HTML
+  jsAnswer.innerHTML = list;
+}
 
-  }
-getPrice();
-var jsAnswer = document.getElementById("answer1");
-jsAnswer.innerHTML = "The average price is $" + avg;
+
+
 //console.log(jsAnswer.innerHTML)
 //now take this answer and get it to inner html answer 1
-var numb = 123.23454;
-numb = numb.toFixed(2);
